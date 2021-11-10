0b2e4d8d483b5fb2b42fe6f415a5140e4e9b2953
Nishita <nishitaparija452@gmail.com>
2021-11-10 09:58:08 +0530

    Strategy_pattern

---
+++ src/ExampleApplication.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+abstract class Strategy {
+    public abstract int execute(int a,int b);
+}
+class ConcreteStrategyadd extends Strategy{
+
+    @Override
+    public int execute(int a, int b) {
+        return(a+b);
+    }
+}
+class ConcreteStrategymulitply extends Strategy{
+
+    @Override
+    public int execute(int a, int b) {
+        return(a*b);
+    }
+}
+class ConcreteStrategysubtract extends Strategy{
+    @Override
+    public int execute(int a,int b ){
+        return(a-b);
+    }
+
+}
+class Context{
+    public Strategy strategy;
+    public void setStrategy(Strategy strategy){
+        this.strategy= strategy;
+    }
+    public int executeStrategy(int a,int b){
+        return strategy.execute(a, b);
+    }
+}
+public class ExampleApplication {
+    public static void main(String[] args) {
+        Context context = new Context();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an operator: ");
+        String operator = scanner.nextLine();
+        switch (operator) {
+            case "*" -> {
+                context.strategy = new ConcreteStrategymulitply();
+                int multiply = context.executeStrategy(5, 13);
+                System.out.println(multiply);
+                ConcreteStrategymulitply obj1 = new ConcreteStrategymulitply();
+                context.setStrategy(new ConcreteStrategymulitply());
+            }
+            case "+" -> {
+                context.strategy = new ConcreteStrategyadd();
+                int add = context.executeStrategy(5, 13);
+                System.out.println(add);
+                ConcreteStrategyadd obj2 = new ConcreteStrategyadd();
+                context.setStrategy(new ConcreteStrategyadd());
+            }
+            case "-" -> {
+                context.strategy = new ConcreteStrategysubtract();
+                int subtract = context.executeStrategy(5, 13);
+                System.out.println(subtract);
+                ConcreteStrategysubtract obj3 = new ConcreteStrategysubtract();
+                context.setStrategy(new ConcreteStrategysubtract());
+            }
+        }
+    }
+}
